edbe1880e57b39efa2a8cad2613256acb128707b
SirAndreRusso <102429266+SirAndreRusso@users.noreply.github.com>
2022-05-28 18:40:00 +0300

    Добавлена логика отображения чисел на дисплее, добавлен ограниченный функционал кнопок с категориями товаров, создан pod file для добавления realm в проект. Данный коммит создан до применения изменений в проект (update to recommended settings Pods.xcodeproj)

---
+++ FirstMoney/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  FirstMoney
+//
+//  Created by Андрей Русин on 28.05.2022.
+//
+
+import UIKit
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var recordCategory: UILabel!
+    @IBOutlet weak var recordCost: UILabel!
+    @IBOutlet weak var recordImage: UIImageView!
+}
--- FirstMoney/ViewController.swift
+++ FirstMoney/ViewController.swift
@@ -16,18 +16,51 @@ class ViewController: UIViewController {
             }
         }
     }
+    @IBOutlet weak var tableView: UITableView!
+    var stillTyping = false
+    var categoryName = ""
+    var displayValue = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func numberPressed(_ sender: UIButton) {
-        if let _ = sender.currentTitle {
-            let number = sender.currentTitle!
-            print(number)
-        displayLabel.text = displayLabel.text! + number
-        }else {
+        
+        if let number = sender.currentTitle {
+            if stillTyping {
+                if displayLabel.text!.count < 13 {
+                    displayLabel.text = displayLabel.text! + number
+                }else{
+                    return}
+            }else {
+                displayLabel.text = number
+                stillTyping = true
+            }
+        } else {
             return}
+    }
+    @IBAction func resetButton(_ sender: UIButton) {
+        displayLabel.text = "0"
+        stillTyping = false
+    }
+    @IBAction func categoryPressed(_ sender: UIButton) {
+        categoryName = sender.currentTitle!
+        displayValue = displayLabel.text!
+        displayLabel.text = "0"
+        stillTyping = false
+        print(categoryName)
+        print(displayValue)
+    }
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
     }
     
 
